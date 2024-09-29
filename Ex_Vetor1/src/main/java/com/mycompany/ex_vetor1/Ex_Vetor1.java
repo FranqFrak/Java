ed9b48314eef379098cb4ea21814ca2a5193ac12
Franquilei <franquilei.oliveira@aluno.senai.br>
2024-09-29 17:17:57 -0300

    Add files via upload

---
+++ Ex_Vetor1/src/main/java/com/mycompany/ex_vetor1/Ex_Vetor1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex_vetor1;
+
+
+
+public class Ex_Vetor1 {
+
+    
+    public static void main(String[] args) {
+      
+     int[] numeros = new int[50];
+     for(int i=0;i<51;i++){
+        
+        System.out.println(i);
+        
+      }
+    }
+}
